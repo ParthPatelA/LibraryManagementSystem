d6baf42e2ef5f831a60aea60bf777b1d5b9d08ea
Parth Patel <parthschool20s@gmail.com>
2024-11-30 17:48:23 -0500

    updated .gitignore

---
+++ LibraryManagementSystem/LibrarySystemForm.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace LibraryManagementSystem
+{
+    partial class LibrarySystemForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dashboardMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.LibrarySummaryGroupBox = new System.Windows.Forms.GroupBox();
+            this.totalUsersLabel = new System.Windows.Forms.Label();
+            this.borrowedBooksLabel = new System.Windows.Forms.Label();
+            this.totalBooksLabel = new System.Windows.Forms.Label();
+            this.btnManageBooks = new System.Windows.Forms.Button();
+            this.btnSearchCatalog = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.btnInventory = new System.Windows.Forms.Button();
+            this.contentPanel = new System.Windows.Forms.Panel();
+            this.dashboardMenuStrip.SuspendLayout();
+            this.LibrarySummaryGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dashboardMenuStrip
+            // 
+            this.dashboardMenuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.dashboardMenuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.dashboardMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.helpToolStripMenuItem1});
+            this.dashboardMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.dashboardMenuStrip.Name = "dashboardMenuStrip";
+            this.dashboardMenuStrip.Size = new System.Drawing.Size(1600, 40);
+            this.dashboardMenuStrip.TabIndex = 0;
+            this.dashboardMenuStrip.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.logoutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(71, 36);
+            this.helpToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(20, 36);
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(84, 36);
+            this.helpToolStripMenuItem1.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(212, 44);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(242, 38);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(133, 44);
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.Location = new System.Drawing.Point(12, 84);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(619, 39);
+            this.welcomeLabel.TabIndex = 1;
+            this.welcomeLabel.Text = "Welcome : ";
+            this.welcomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LibrarySummaryGroupBox
+            // 
+            this.LibrarySummaryGroupBox.Controls.Add(this.totalUsersLabel);
+            this.LibrarySummaryGroupBox.Controls.Add(this.borrowedBooksLabel);
+            this.LibrarySummaryGroupBox.Controls.Add(this.totalBooksLabel);
+            this.LibrarySummaryGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LibrarySummaryGroupBox.Location = new System.Drawing.Point(19, 184);
+            this.LibrarySummaryGroupBox.Name = "LibrarySummaryGroupBox";
+            this.LibrarySummaryGroupBox.Size = new System.Drawing.Size(682, 197);
+            this.LibrarySummaryGroupBox.TabIndex = 2;
+            this.LibrarySummaryGroupBox.TabStop = false;
+            this.LibrarySummaryGroupBox.Text = "Library Summary";
+            // 
+            // totalUsersLabel
+            // 
+            this.totalUsersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalUsersLabel.Location = new System.Drawing.Point(6, 89);
+            this.totalUsersLabel.Name = "totalUsersLabel";
+            this.totalUsersLabel.Size = new System.Drawing.Size(282, 39);
+            this.totalUsersLabel.TabIndex = 2;
+            this.totalUsersLabel.Text = "Total Users:";
+            this.totalUsersLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // borrowedBooksLabel
+            // 
+            this.borrowedBooksLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.borrowedBooksLabel.Location = new System.Drawing.Point(6, 125);
+            this.borrowedBooksLabel.Name = "borrowedBooksLabel";
+            this.borrowedBooksLabel.Size = new System.Drawing.Size(282, 39);
+            this.borrowedBooksLabel.TabIndex = 1;
+            this.borrowedBooksLabel.Text = "Books Borrowed:";
+            this.borrowedBooksLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // totalBooksLabel
+            // 
+            this.totalBooksLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalBooksLabel.Location = new System.Drawing.Point(6, 50);
+            this.totalBooksLabel.Name = "totalBooksLabel";
+            this.totalBooksLabel.Size = new System.Drawing.Size(282, 39);
+            this.totalBooksLabel.TabIndex = 0;
+            this.totalBooksLabel.Text = "Total Books:";
+            this.totalBooksLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnManageBooks
+            // 
+            this.btnManageBooks.Location = new System.Drawing.Point(827, 184);
+            this.btnManageBooks.Name = "btnManageBooks";
+            this.btnManageBooks.Size = new System.Drawing.Size(236, 72);
+            this.btnManageBooks.TabIndex = 4;
+            this.btnManageBooks.Text = "Manage Books";
+            this.btnManageBooks.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchCatalog
+            // 
+            this.btnSearchCatalog.Location = new System.Drawing.Point(1106, 184);
+            this.btnSearchCatalog.Name = "btnSearchCatalog";
+            this.btnSearchCatalog.Size = new System.Drawing.Size(236, 72);
+            this.btnSearchCatalog.TabIndex = 5;
+            this.btnSearchCatalog.Text = "Search Catalog";
+            this.btnSearchCatalog.UseVisualStyleBackColor = true;
+            // 
+            // btnReports
+            // 
+            this.btnReports.Location = new System.Drawing.Point(827, 311);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(236, 72);
+            this.btnReports.TabIndex = 6;
+            this.btnReports.Text = "Reports";
+            this.btnReports.UseVisualStyleBackColor = true;
+            // 
+            // btnInventory
+            // 
+            this.btnInventory.Location = new System.Drawing.Point(1106, 309);
+            this.btnInventory.Name = "btnInventory";
+            this.btnInventory.Size = new System.Drawing.Size(236, 72);
+            this.btnInventory.TabIndex = 7;
+            this.btnInventory.Text = "Inventory";
+            this.btnInventory.UseVisualStyleBackColor = true;
+            // 
+            // contentPanel
+            // 
+            this.contentPanel.Location = new System.Drawing.Point(31, 432);
+            this.contentPanel.Name = "contentPanel";
+            this.contentPanel.Size = new System.Drawing.Size(1311, 253);
+            this.contentPanel.TabIndex = 8;
+            // 
+            // LibrarySystemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.contentPanel);
+            this.Controls.Add(this.btnManageBooks);
+            this.Controls.Add(this.btnSearchCatalog);
+            this.Controls.Add(this.btnReports);
+            this.Controls.Add(this.btnInventory);
+            this.Controls.Add(this.LibrarySummaryGroupBox);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.dashboardMenuStrip);
+            this.MainMenuStrip = this.dashboardMenuStrip;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "LibrarySystemForm";
+            this.Text = "Library System Management - Dashboard";
+            this.dashboardMenuStrip.ResumeLayout(false);
+            this.dashboardMenuStrip.PerformLayout();
+            this.LibrarySummaryGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip dashboardMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.GroupBox LibrarySummaryGroupBox;
+        private System.Windows.Forms.Label totalUsersLabel;
+        private System.Windows.Forms.Label borrowedBooksLabel;
+        private System.Windows.Forms.Label totalBooksLabel;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button btnManageBooks;
+        private System.Windows.Forms.Button btnSearchCatalog;
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.Button btnInventory;
+        private System.Windows.Forms.Panel contentPanel;
+    }
+}
---
+++ LibraryManagementSystem/LoginForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace LibraryManagementSystem
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeMessageLabel = new System.Windows.Forms.Label();
+            this.loginPromptLabel = new System.Windows.Forms.Label();
+            this.namePromptLabel = new System.Windows.Forms.Label();
+            this.emailPromptLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.validateButton = new System.Windows.Forms.Button();
+            this.displayMessage = new System.Windows.Forms.Label();
+            this.roleComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // welcomeMessageLabel
+            // 
+            this.welcomeMessageLabel.AutoSize = true;
+            this.welcomeMessageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeMessageLabel.Location = new System.Drawing.Point(428, 115);
+            this.welcomeMessageLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.welcomeMessageLabel.Name = "welcomeMessageLabel";
+            this.welcomeMessageLabel.Size = new System.Drawing.Size(781, 63);
+            this.welcomeMessageLabel.TabIndex = 0;
+            this.welcomeMessageLabel.Text = "Welcome to our library system";
+            // 
+            // loginPromptLabel
+            // 
+            this.loginPromptLabel.AutoSize = true;
+            this.loginPromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginPromptLabel.Location = new System.Drawing.Point(658, 260);
+            this.loginPromptLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.loginPromptLabel.Name = "loginPromptLabel";
+            this.loginPromptLabel.Size = new System.Drawing.Size(283, 44);
+            this.loginPromptLabel.TabIndex = 1;
+            this.loginPromptLabel.Text = "Please sign-in ";
+            // 
+            // namePromptLabel
+            // 
+            this.namePromptLabel.AutoSize = true;
+            this.namePromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.namePromptLabel.Location = new System.Drawing.Point(314, 417);
+            this.namePromptLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.namePromptLabel.Name = "namePromptLabel";
+            this.namePromptLabel.Size = new System.Drawing.Size(349, 44);
+            this.namePromptLabel.TabIndex = 2;
+            this.namePromptLabel.Text = "Enter your name : ";
+            // 
+            // emailPromptLabel
+            // 
+            this.emailPromptLabel.AutoSize = true;
+            this.emailPromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailPromptLabel.Location = new System.Drawing.Point(314, 533);
+            this.emailPromptLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.emailPromptLabel.Name = "emailPromptLabel";
+            this.emailPromptLabel.Size = new System.Drawing.Size(347, 44);
+            this.emailPromptLabel.TabIndex = 3;
+            this.emailPromptLabel.Text = "Enter your email : ";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(788, 427);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(448, 31);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(788, 540);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(448, 31);
+            this.emailTextBox.TabIndex = 5;
+            // 
+            // validateButton
+            // 
+            this.validateButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.validateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.validateButton.Location = new System.Drawing.Point(538, 713);
+            this.validateButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.validateButton.Name = "validateButton";
+            this.validateButton.Size = new System.Drawing.Size(488, 73);
+            this.validateButton.TabIndex = 6;
+            this.validateButton.Text = "Validate";
+            this.validateButton.UseVisualStyleBackColor = false;
+            this.validateButton.Click += new System.EventHandler(this.validateButton_Click);
+            // 
+            // displayMessage
+            // 
+            this.displayMessage.AutoSize = true;
+            this.displayMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayMessage.ForeColor = System.Drawing.Color.Red;
+            this.displayMessage.Location = new System.Drawing.Point(550, 633);
+            this.displayMessage.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.displayMessage.Name = "displayMessage";
+            this.displayMessage.Size = new System.Drawing.Size(0, 37);
+            this.displayMessage.TabIndex = 7;
+            // 
+            // roleComboBox
+            // 
+            this.roleComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roleComboBox.FormattingEnabled = true;
+            this.roleComboBox.Items.AddRange(new object[] {
+            "Librarian",
+            "Customer"});
+            this.roleComboBox.Location = new System.Drawing.Point(322, 341);
+            this.roleComboBox.Name = "roleComboBox";
+            this.roleComboBox.Size = new System.Drawing.Size(317, 39);
+            this.roleComboBox.TabIndex = 8;
+            this.roleComboBox.Text = "Type of User";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.roleComboBox);
+            this.Controls.Add(this.displayMessage);
+            this.Controls.Add(this.validateButton);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.emailPromptLabel);
+            this.Controls.Add(this.namePromptLabel);
+            this.Controls.Add(this.loginPromptLabel);
+            this.Controls.Add(this.welcomeMessageLabel);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "LoginForm";
+            this.Text = "Libreary Management System - Login Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcomeMessageLabel;
+        private System.Windows.Forms.Label loginPromptLabel;
+        private System.Windows.Forms.Label namePromptLabel;
+        private System.Windows.Forms.Label emailPromptLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Button validateButton;
+        private System.Windows.Forms.Label displayMessage;
+        private System.Windows.Forms.ComboBox roleComboBox;
+    }
+}
+
